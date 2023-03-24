94d264e1e1efa2997f5ad49a4e7e4735539d441b
davide <davidecolombi85@gmail.com>
2023-03-24 16:29:36 +0100

    AVEVO SBAGLIATO A SCRIVERE SELECTION

--- Assets/Scripts/SelectionController.cs
+++ Assets/Scripts/SelectionController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelctionController : MonoBehaviour
+public class SelectionController : MonoBehaviour
 {
     [SerializeField] Transform cameraTrans;
     [SerializeField] LayerMask layerMask;
