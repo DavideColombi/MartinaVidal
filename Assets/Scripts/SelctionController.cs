02ce7ad87e62b1245086c02628c840340e85ad04
davide <davidecolombi85@gmail.com>
2023-03-24 15:52:21 +0100

    raycast aggiornato

--- Assets/Scripts/SelctionController.cs
+++ Assets/Scripts/SelctionController.cs
@@ -7,6 +7,7 @@ public class SelctionController : MonoBehaviour
     [SerializeField] Transform cameraTrans;
     [SerializeField] LayerMask layerMask;
     private GameObject lastObject;
+    float timerSpegnimento = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +22,28 @@ public class SelctionController : MonoBehaviour
         if (Physics.Raycast(cameraTrans.position, cameraTrans.forward, out hit, Mathf.Infinity, layerMask))
         {
             //hittato
+            for(int i=0; i < hit.transform.childCount; i++)
+            {
+                GameObject canvas = hit.transform.GetChild(i).gameObject;
+                if(canvas.name == "Canvas")
+                {
+                    timerSpegnimento = 0;
+                    canvas.SetActive(true);
+                    lastObject = canvas;
+                }
+            }
+            
+            
         }
         else
         {
+            timerSpegnimento += Time.deltaTime;
             //non hittato
+            if(lastObject != null && timerSpegnimento >= 4f)
+            {
+                lastObject.SetActive(false);
+                lastObject = null;
+            }
         }
     }
 }
